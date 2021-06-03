584e72d19fb63c0332285dce11f8d9047621cef1
Roman Kalnytskyi <moranibaca@gmail.com>
2021-06-03 10:22:04 +0300

    Always use latest imagemagick version

--- .travis.install_imagemagick.sh
+++ .travis.install_imagemagick.sh
@@ -5,12 +5,11 @@ sudo apt-get remove imagemagick && sudo apt-get autoremove
 sudo apt-get install build-essential
 sudo apt-get build-dep imagemagick -y
 
-im_version='7.0.11-13'
+wget "http://www.imagemagick.org/download/ImageMagick.tar.gz"
+mkdir ImageMagick
+tar xzvf ImageMagick.tar.gz -C ./ImageMagick
 
-wget "http://www.imagemagick.org/download/ImageMagick-$im_version.tar.gz"
-tar xzvf "ImageMagick-$im_version.tar.gz"
-
-cd "ImageMagick-$im_version/"
+cd ImageMagick/*/
 ./configure
 
 make
